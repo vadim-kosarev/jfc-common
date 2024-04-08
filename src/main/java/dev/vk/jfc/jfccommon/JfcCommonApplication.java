89fdb2d7aab6df53a1a1c12a7ab7a17114861c4a
Vadim Kosarev <vadim.kosarev@gmail.com>
2024-04-08 08:58:07 +0300

    Initial commit

---
+++ src/main/java/dev/vk/jfc/jfccommon/JfcCommonApplication.java
@@ -0,0 +1,13 @@
+package dev.vk.jfc.jfccommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfcCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfcCommonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vk/jfc/jfccommon/JfcCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vk.jfc.jfccommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfcCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
