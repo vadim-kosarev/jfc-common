6a7c3ea4b2cefc181aecde902e5fe125833c89f3
Vadim Kosarev <vadim.kosarev@gmail.com>
2024-04-09 00:26:33 +0300

    Moving module structure

---
+++ src/main/java/dev/vk/jfc/jfccommon/dto/ImageMessage.java
@@ -0,0 +1,13 @@
+package dev.vk.jfc.jfccommon.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageMessage {
+}
