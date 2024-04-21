a19fa04994fadcedea17bccad3ed840e1af4415a
Vadim Kosarev <vadim.kosarev@gmail.com>
2024-04-21 20:30:40 +0300

    Added constants

--- src/main/java/dev/vk/jfc/jfccommon/Jfc.java
+++ src/main/java/dev/vk/jfc/jfccommon/Jfc.java
@@ -9,7 +9,11 @@ public abstract class Jfc {
     public static final String K_LOCALID = "localID";
     public static final String K_FRAME_STORAGE_PATH = "frameStoragePath";
     public static final String K_UUID = "uuid";
+    public static final String K_PARENT_UUID = "parentUuid";
     public static final String K_S3PATH = "s3Path";
     public static final String K_MESSAGE_TYPE = "message-type";
 
+    public static final String TP_PROCESSED_FRAME_FACES = "processed-frame-faces";
+    public static final String TP_PROCESSED_FRAME_FACE = "processed-frame-face";
+    public static final String TP_PROCESSED_FRAME = "processed-frame";
 }
